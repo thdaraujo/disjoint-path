7cfd4cd414f7772267a8810db54b5821eda27657
taraujo <thiago@carambola.com.vc>
2014-05-15 21:56:03 -0300

    projeto inicial

---
+++ src/KPathFinderTest.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+
+/**
+ * @author thiagoaraujo
+ *
+ */
+public class KPathFinderTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		printProjectName();
+	}
+	
+	private static void printProjectName() {
+		System.out.println("##   EP4 - Path Finder   ##");
+		System.out.println("##### Thiago Araujo #######");
+		System.out.println("###########################");
+		System.out.println("");
+	}
+
+}
