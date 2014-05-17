cdbdda56d34f424aff226ac0daf564e67df64477
taraujo <thiago@carambola.com.vc>
2014-05-17 18:35:53 -0300

    teste basico

--- src/Graph.java
+++ src/Graph.java
@@ -1,6 +1,7 @@
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.LinkedList;
+import java.util.Map;
 
 import graph.Edge;
 import graph.IGraph;
@@ -11,15 +12,15 @@ public class Graph implements IGraph {
 	private HashMap<Node, LinkedList<Node>> adjNodes;
 	
 	private int V;
-	private HashMap<Node, Node> nodes;
+	private HashMap<Integer, Node> nodes;
 	
 	private int E;
-	private HashMap<Edge, Edge> edges;
+	private HashMap<Integer, Edge> edges;
 	
 	public Graph(){
 		this.adjNodes = new HashMap<Node, LinkedList<Node>>();
-		this.nodes = new HashMap<Node, Node>();
-		this.edges = new HashMap<Edge, Edge>();
+		this.nodes = new HashMap<Integer, Node>();
+		this.edges = new HashMap<Integer, Edge>();
 	}
 	
 	public int V(){
@@ -32,11 +33,11 @@ public class Graph implements IGraph {
 	
 	@Override
 	public boolean containsEdge(Edge arg0) {
-		return this.edges.containsKey(arg0);
+		return this.edges.containsKey(arg0.hashCode());
 	}
 	
 	public boolean containsNode(Node arg0){
-		return this.nodes.containsKey(arg0);
+		return this.nodes.containsKey(arg0.hashCode());
 	}
 
 	@Override
@@ -51,7 +52,7 @@ public class Graph implements IGraph {
 			System.out.println("node not found!");
 			return null;
 		}
-		return this.adjNodes.get(arg0);
+		return this.adjNodes.get(arg0.hashCode());
 	}
 
 	@Override
@@ -60,7 +61,7 @@ public class Graph implements IGraph {
 	}
 	
 	public void addNode(Node n){
-		this.nodes.put(n, n);
+		this.nodes.put(n.hashCode(), n);
 		this.adjNodes.put(n, new LinkedList<Node>());
 		this.V++;
 	}
@@ -75,7 +76,25 @@ public class Graph implements IGraph {
 		}
 		
 		this.adjNodes.get(from).add(to);
-		this.edges.put(e, e);
+		this.edges.put(e.hashCode(), e);
 		this.E++;
 	}
+	
+	public String toString() {
+        StringBuilder s = new StringBuilder();
+        String NEWLINE = System.getProperty("line.separator");
+        s.append(this.V + " vertices, " + this.E + " edges " + NEWLINE);
+        
+        for (Map.Entry<Node, LinkedList<Node>> entry : this.adjNodes.entrySet()) {
+            Node key = entry.getKey();
+            LinkedList<Node> adjacentList = entry.getValue();
+            
+            s.append(key.getLabel().toString() + ": ");
+            for (Node w : adjacentList) {
+                s.append(w.getLabel().toString() + " ");
+            }
+            s.append(NEWLINE);
+        }
+        return s.toString();
+    }
 }
--- src/KPathFinderTest.java
+++ src/KPathFinderTest.java
@@ -1,3 +1,8 @@
+import java.util.LinkedList;
+
+import graph.Edge;
+import graph.Node;
+
 /**
  * 
  */
@@ -12,9 +17,50 @@ public class KPathFinderTest {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
 		printProjectName();
+		
+		Graph G = new Graph();
+		
+		LinkedList<Node> nodes = getNodes(10);
+		for(Node n : nodes){
+			System.out.println(n.getLabel() + ": " +  n.hashCode());
+			G.addNode(n);
+		}
+		
+		LinkedList<Edge> edges = getEdges(new LinkedList<Node>(G.getNodes()));
+		for (Edge e : edges) {
+			System.out.println(e.toString() + ": " +  e.hashCode());
+			G.addEdge(e);
+		}
+		
+		System.out.println(G.toString());
+		
+	}
+	
+	public static LinkedList<Node> getNodes(int count){
+		LinkedList<Node> nodes = new LinkedList<Node>();
+		
+		for(int i = 1; i <= count; i++){
+			nodes.add(new Node(i));
+		}
+		
+		return nodes;
+	}
+	
+	public static LinkedList<Edge> getEdges(LinkedList<Node> nodes){
+		LinkedList<Edge> edges = new LinkedList<Edge>();
+		
+		while(!nodes.isEmpty()){
+			Node from = nodes.pop();
+			Node to = nodes.pop();
+			
+			if(from != null && to != null){
+				Edge e = new Edge(from, to);
+				edges.add(e);
+			}
+		}
+		
+		return edges;
 	}
 	
 	private static void printProjectName() {
